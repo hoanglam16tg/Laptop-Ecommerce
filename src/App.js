4ae8f0c50da713fffccfa9489d44d2768f52a746
hoanglam16tg <hoanglam16tg@gmail.com>
2021-08-20 20:44:20 +0700

    update source code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import "antd/dist/antd.css";
 import "./styles/App.scss";
 import Home from "./screens/Home";
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Home />
+      </Router>
     </>
   );
 }
