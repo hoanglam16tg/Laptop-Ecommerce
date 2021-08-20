a0ff6316f2873925d26dbe129f805e98a5b1a98f
hoanglam16tg <hoanglam16tg@gmail.com>
2021-08-20 11:37:24 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles/App.scss'
+function App() {
+  return (
+    <div className='container'>
+      test
+    </div>
+  );
+}
+
+export default App;
