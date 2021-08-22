b7a49df4b7c6133e87afe02298bd4be004225e17
Hàn Kiêu <nguyentuankiet0105@gmail.com>
2021-08-22 22:15:31 +0700

    update src body

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/layout/header/index";
 import Footer from "../components/layout/Footer";
+import Body from "../components/layout/Body";
 const Home = () => {
   return (
     <>
       <Header />
+      <Body />
       <Footer />
     </>
   );
