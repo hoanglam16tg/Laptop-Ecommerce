912681a9e2327151a55af369ec5bb8835434281c
Hàn Kiêu <nguyentuankiet0105@gmail.com>
2021-08-24 21:08:33 +0700

    update code body

---
+++ src/components/common/BodyDetails/DataCard.js
@@ -0,0 +1,90 @@
+export const dataLaptop = [
+  {
+    status: 'In Stock',
+    src: 'https://lh3.googleusercontent.com/MaNYTTv-4gkgptnl12eLDeA-Q6GvFaQLwqwGrhRCk2atMwD1blGaBNITqaEE8EuYlrU_2k4f6jxY4f2tOZSy7JRzRgXgJ5U=rw-w300',
+    content: 'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On',
+    title:'Laptop Dell',
+    price:50,
+  },
+  {
+    status: 'Check availability',
+    src: 'https://lh3.googleusercontent.com/0OykvKy1Tq_Xwq_tUI9KgCckUOF5jh2n2b7nOyw1Ep8684JlEg7PP4GDGEYsEgPyNt3cZUCqLK6dtTIbjZY50Vui_7HkYCY=rw-w300',
+    content: 'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On',
+    title:'Laptop 2',
+    price:49,
+  },
+  {
+    status: 'In Stock',
+    src: 'https://lh3.googleusercontent.com/-oZ_FNBfq-jU5eT4LFhySIWzc3GwC0482-ky3ON17NR1a2njS7fXxGT4L-Y916XDSaQJZMZSP-E1qHvihqq4OSjMQUcd7Is=rw-w300',
+    content: 'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On',
+    title:'Macbook',
+    price:100,
+  },
+  {
+    status: 'Check availability',
+    src: 'https://lh3.googleusercontent.com/unMbspoftcygBpi0SEOKZ_gvVpWViCs8oGjc8gO1ZfxEtuvN-c3SZ_RGbOjIVKiujTCNVhynyIOGlh6qLeOuvLy0eInCu7uE=rw-w300',
+    content: 'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On',
+    title:'ASUS',
+    price:60,
+  },
+];
+export const dataCPU = [
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/gcXtjI_Kr1mJLabyrjRpwoSom5a7wzm-I0wwuRIOrUCDO9-_JY1GOkGkGPzP8f5OhtB5iaUgDkl-EgFdY_A=rw-w300', 
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'CPU INTEL Core i7-10700K', 
+    price:'200', 
+  },
+  {
+    status:'In Stock', 
+    src:'https://lh3.googleusercontent.com/M1mCTDGefGmDf9_mdG1ffV71HkzcfXJFRqRzF8K94ZVf7vyoNO0yoTBt0TA2zVDy1fn-VFo0d0pNHGA8l8c=rw-w300', 
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'CPU INTEL Core i3-10700K', 
+    price:'300', 
+  },
+  {
+    status:'In Stock', 
+    src:'https://lh3.googleusercontent.com/EFoe1bzBy7nFLbUHO_TB9FxxH7ZhLFqFb57hggQ_QfrmrC3bH25Uum31xQW2HY5y-8U-lLEMIvDrxK4VeOw=rw-w300', 
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'CPU INTEL Core i7-10700K', 
+    price:'400', 
+  },
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/LkwcaIK0uES136Z1JK5GnhTGnLt8I9UwLmP0UtTuzSldGcDPlDzciZ5E1GOVbpH5dR3eTgktTqF5eDW2mw=rw-w300', 
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'CPU INTEL Core i9-10700K', 
+    price:'500', 
+  },
+];
+export const dataScreen = [
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/nmxUcOhEU29rmO3xMbmTgVNm2jQPlTAKl9G3d9Hs461jk8_j6pE06QBOs_eTTdXLn1ZvGdvDCVpRQA-eSZuvWftUywqdDA5T=rw-w300', 
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'Screen SAMSUNG 21.5"', 
+    price:'100', 
+  },
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/kAK3d33UHijquJtxzrCm2klfAqShHBrlDVIfsvOWhXJKazeurPNDRm6O6ssNx65Ocj8e88yd5ns82Aa5LnW7LPPIwY2FAT4R=rw-w300',
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'Screen DELL 19.5"', 
+    price:'250', 
+  },
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/0_o2q2NNaCqfQqN_L8vWtU88IOwNXCLvbA2e7YK30v1GWZKwjdu2QK9i4xFelhBObAXwhofauWcCm9-6KtPHpLdwRLpUP28=rw-w300',
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'Screen LCD SAMSUNG 23.8"', 
+    price:'300', 
+  },
+  {
+    status:'Check availability', 
+    src:'https://lh3.googleusercontent.com/m2BRFggSZAa0aF1X3E6q_FeY7w2cwBZEV2cyOBFV6AMskJDK9oh7A51N46dT1SJE8bMzIcMZglDoyCNXuNM=rw-w300',
+    content:'EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On', 
+    title:'Screen LCD LG 27"', 
+    price:'350', 
+  },
+]
--- src/components/common/BodyDetails/ProductCard.js
+++ src/components/common/BodyDetails/ProductCard.js
@@ -1,8 +1,13 @@
+import '../../../styles/_productCard.scss';
+
 const ProductCard = (props) => {
   return (
-    <div>
+    <div className="productCard">
       <h3>{props.status}</h3>
       <img src={props.src} alt={props.alt} />
+      <h2>{props.title}</h2>
+      <p>{props.content}...</p>
+      <h2>${props.price}.00</h2>
     </div>
   );
 };
